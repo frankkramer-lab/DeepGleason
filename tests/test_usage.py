8f5ad293466076a1e68f98606de6927ebbf15ae9
muellerdo <domey038@live.de>
2024-01-23 21:59:05 +0100

    test: added unittesting for input directory support

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -15,9 +15,9 @@
 #  You should have received a copy of the GNU General Public License           #
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 # =============================================================================#
-# -----------------------------------------------------#
+#------------------------------------------------------#
 #                    Library imports                   #
-# -----------------------------------------------------#
+#------------------------------------------------------#
 # External libraries
 import unittest
 import tempfile
@@ -26,18 +26,18 @@ import os
 import json
 import zipfile
 
-# -----------------------------------------------------#
+#------------------------------------------------------#
 #                 Public Kaggle Token                 #
-# -----------------------------------------------------#
+#------------------------------------------------------#
 # public token, please do not misuse
 # should be only used for the testing suite of the DeepGleason software
 token = {"username": "misitlab", "key": "503c4a25182320957f0464a8551d3f48"}
 # testing file
 testing_file = "train_images/006f6aa35a78965c92fffd1fbd53a058.tiff"
 
-# -----------------------------------------------------#
+#------------------------------------------------------#
 #                   Unittest: Usage                    #
-# -----------------------------------------------------#
+#------------------------------------------------------#
 class DeepGleasonUsage(unittest.TestCase):
     # Create random imaging and classification data
     @classmethod
@@ -64,17 +64,17 @@ class DeepGleasonUsage(unittest.TestCase):
         with zipfile.ZipFile(path_wsi_zip) as z:
             z.extractall(self.tmp_data.name)
 
-    # -------------------------------------------------#
+    #--------------------------------------------------#
     #              Run DeepGleason - Help              #
-    # -------------------------------------------------#
+    #--------------------------------------------------#
     def test_runDeepGleason_help(self):
         path_wsi = os.path.join(self.tmp_data.name, testing_file.split("/")[-1])
         res = os.system("python code/main.py --help")
         self.assertEqual(res, 0)
 
-    # -------------------------------------------------#
+    #--------------------------------------------------#
     #              Run DeepGleason - Basic             #
-    # -------------------------------------------------#
+    #--------------------------------------------------#
     def test_runDeepGleason_basic(self):
         path_wsi = os.path.join(self.tmp_data.name, testing_file.split("/")[-1])
         path_output = os.path.join(self.tmp_data.name)
@@ -89,9 +89,9 @@ class DeepGleasonUsage(unittest.TestCase):
                         testing_file.split("/")[-1].split(".")[0] + \
                             "_gleason.tiff")))
 
-    # -------------------------------------------------#
+    #--------------------------------------------------#
     #            Run DeepGleason - Overlay             #
-    # -------------------------------------------------#
+    #--------------------------------------------------#
     def test_runDeepGleason_overlay(self):
         path_wsi = os.path.join(self.tmp_data.name, testing_file.split("/")[-1])
         path_output = os.path.join(self.tmp_data.name)
@@ -105,3 +105,24 @@ class DeepGleasonUsage(unittest.TestCase):
         self.assertTrue(os.path.exists(os.path.join(path_output, 
                         testing_file.split("/")[-1].split(".")[0] + \
                             "_gleason.tiff")))
+
+    #--------------------------------------------------#
+    #            Run DeepGleason - Multiple            #
+    #--------------------------------------------------#
+    def test_runDeepGleason_multiple(self):
+        # Create temporary directory-based imaging data for multiple slides
+        tmp_data_multiple= tempfile.TemporaryDirectory(prefix="tmp.DeepGleason.")
+        path_wsi = os.path.join(self.tmp_data.name, testing_file.split("/")[-1])
+        os.symlink(path_wsi, os.path.join(tmp_data_multiple.name, "one.tiff"))
+        os.symlink(path_wsi, os.path.join(tmp_data_multiple.name, "two.tiff"))
+
+        path_output = os.path.join(tmp_data_multiple.name)
+        path_preds = os.path.join(tmp_data_multiple.name, "preds.csv")
+        path_model = os.path.join("models", "model.DenseNet121.hdf5")
+        res = os.system("python code/main.py -i " + tmp_data_multiple.name + " -o " + \
+                        path_output + " --model " + path_model + \
+                        " -p " + path_preds)
+        self.assertEqual(res, 0)
+        self.assertTrue(os.path.exists(path_preds))
+        self.assertTrue(os.path.exists(os.path.join(path_output, "one" + "_gleason.tiff")))
+        self.assertTrue(os.path.exists(os.path.join(path_output, "two" + "_gleason.tiff")))
